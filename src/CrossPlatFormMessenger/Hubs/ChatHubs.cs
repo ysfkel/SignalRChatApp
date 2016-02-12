44d418f9400b015eef5de3580292c80d92a9386a
Yusuf Kelo <yusufkel@gmail.com>
2016-02-12 17:47:23 +0400

    initial

---
+++ src/CrossPlatFormMessenger/Hubs/ChatHubs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace CrossPlatFormMessenger.Hubs
+{
+    public class ChatHubs:Hub
+    {
+       // public void SendMessage(ChatMessage message)
+              public void SendMessage(ChatMessage message)
+        {
+            Clients.All.GetMessage(message);
+        }
+
+    }
+
+    public class ChatMessage
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
